5f4763507fbe9b9331f7c5f53757681676716fd8
Laureano Martin <mjuanlaureano@gmail.com>
2025-01-05 21:18:54 -0300

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from './components/Hero'
-//import Projects from './components/Projects'
+import Projects from './components/Projects'
 import Footer from './components/Footer'
 import PixelatedStars from './components/PixelatedStars'
 
